60a08acddf0803c0d6ab2fc37b7234053b557a7d
Arihant Jain <arihant.jain67@gmail.com>
2025-07-06 18:03:16 +0530

    change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello, Wordd!</h1>");
+  res.send("<h1>Hello, Wordy!</h1>");
 });
 
 app.listen(3000, "0.0.0.0", () => {
