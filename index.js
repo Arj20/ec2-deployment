b881c6f503b2085b05a78a8ecc6fd1e3d4baf55b
Arihant Jain <arihantjain@Arihants-MacBook-Air.local>
2025-07-06 17:39:49 +0530

    make app public

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello, World!</h1>");
+  res.send("<h1>Hello, Wordd!</h1>");
 });
 
-app.listen(3000, () => console.log("hello I am under the water!"));
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running");
+});
